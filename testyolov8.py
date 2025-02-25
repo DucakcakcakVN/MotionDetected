849ecff16a4acdfee26ecdda20dc8422f6413093
DucakcakcakVN <123886217+DucakcakcakVN@users.noreply.github.com>
2025-02-25 21:35:19 +0700

    Update testyolov8.py

--- testyolov8.py
+++ testyolov8.py
@@ -13,9 +13,8 @@ while cap.isOpened() and not stop_button_pressed:
         st.write("Không thể truy cập webcam.")
         break
     frame=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_placeholder.image(frame)
+    frame_placeholder.image(frame, CHANNELS="RGB")
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q') or stop_button_pressed:
         break
 cap.release()
-
